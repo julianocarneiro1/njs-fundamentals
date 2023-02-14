1324c4c4c3d8130601a0ba10997679ffbaf1f29e
Juliano <julianoomc@gmail.com>
2023-02-14 16:51:11 -0300

    Inserindo nodemon e middleware

---
+++ 6_EXPRESS/5_middleware/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3000 //variável ambiente
+
+const path = require('path')
+const basePath = path.join(__dirname, 'templates')
+
+const checkAuth = function(req, res, next) {
+    req.authStatus = true
+
+    if (req.authStatus) {
+        console.log('Está logado, pode continuar')
+        next() //deixar prosseguir
+    } else {
+        console.log('Não está logado, faça o login para continuar')
+        next() //redirecionar para página de login
+    }
+}
+
+app.use(checkAuth)
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`)
+})
